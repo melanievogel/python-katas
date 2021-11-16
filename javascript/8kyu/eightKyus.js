993b63916bbf153bc78e8dd6d74567312ee513ec
melanie <vogel.melanie@outlook.com>
2021-11-16 20:35:24 +0100

    feat: Lovefun kata.

--- javascript/8kyu/eightKyus.js
+++ javascript/8kyu/eightKyus.js
@@ -85,3 +85,16 @@ function even_or_odd(number) {
 function opposite(number) {
     return number < 0 ? Math.abs(number) : number * -1;
 }
+
+function lovefunc(flower1, flower2){
+    if(flower1 % 2 != 0 && flower2 % 2 == 0){
+      return true;
+    }
+    else if (flower1 % 2 == 0 && flower2 % 2 != 0){
+      return true;
+    }
+    else if (flower1 % 2 == 0 && flower2 % 2 == 0){
+      return false;
+    }
+    return false;
+  }
