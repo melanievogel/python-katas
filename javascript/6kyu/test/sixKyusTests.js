298c34fef35ce24870b07e856d432dd5a990f41e
melanie <vogel.melanie@outlook.com>
2022-01-07 22:56:12 +0100

    feat: Refactored test file.

---
+++ javascript/6kyu/test/sixKyusTests.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+var sixKyus = require('../sixKyus.js');
+
+describe("sixKyus", () => {
+    it("toCamelCaseTests", () => {
+        expect(sixKyus.toCamelCase('')).to.equal('', "An empty string was provided but not returned");
+        expect(sixKyus.toCamelCase("the_stealth_warrior")).equal("theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value");
+        expect(sixKyus.toCamelCase("The-Stealth-Warrior")).equal("TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value");
+        expect(sixKyus.toCamelCase("A-B-C")).equal("ABC", "toCamelCase('A-B-C') did not return correct value");
+    });
+});
